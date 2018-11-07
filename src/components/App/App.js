09baa6f6f6098f8a9751ecef203e22c73b5ccf4a
Evgeniy Guzhikhin <gpont97@gmail.com>
2018-11-07 21:06:55 +0700

    Move App to components, replace Canvas with WebGLCanvas

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
-import Canvas from './Canvas';
+import WebGLCanvas from '../WebGLCanvas/WebGLCanvas';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Canvas
-          size={750}
-          scale={15}
-          gridSize={25}
-          font={'9px Arial'}
-        />
+				<WebGLCanvas/>
       </div>
     );
   }
