9bc88feec706554de7c8445cfde0b6de127e135a
Dmitry Gutman <fryngies@tomengine.ru>
2018-11-12 16:58:21 +0700

    add missing changes for typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
-import Canvas from './Canvas';
-import './App.css';
+import * as React from 'react';
 
-class App extends Component {
-  render() {
+import './App.css';
+// @ts-ignore
+import Canvas from './Canvas.js';
+
+function App() {
     return (
-      <div className="App">
-        <Canvas
-          size={750}
-          scale={15}
-          gridSize={25}
-          font={'9px Arial'}
-        />
-      </div>
+        <div className="App">
+            <Canvas
+                size={750}
+                scale={15}
+                gridSize={25}
+                font={'9px Arial'}
+            />
+        </div>
     );
-  }
 }
 
 export default App;
