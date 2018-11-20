8f881e5339f1b1278e2ed5dcabddd5893da1318a
Dmitry Gutman <fryngies@tomengine.ru>
2018-11-21 01:26:22 +0700

    fix imports

--- src/glTasks/glLinesAndTrianglesHof.ts
+++ src/glTasks/glLinesAndTrianglesHof.ts
@@ -1,4 +1,4 @@
-import { bindShadersToBuffers, initBuffers, initShaderProgram } from '../components/WebGLCanvas/utils';
+import { bindShadersToBuffers, initBuffers, initShaderProgram } from '../glUtils';
 import { GlPrimitives } from '../glUtils/types';
 
 import { TaskFuncBaseProps } from './types';
