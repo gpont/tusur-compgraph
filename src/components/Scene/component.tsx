2aa04e6d1ae2775606a68f7e42c1a85e72b9a8cc
Dmitry Gutman <fryngies@tomengine.ru>
2018-11-21 01:21:40 +0700

    handle resize and hidpi

--- src/components/Scene/component.tsx
+++ src/components/Scene/component.tsx
@@ -4,6 +4,8 @@ import * as React from 'react';
 import tasks from '../../glTasks';
 import { RootTasksType, TaskFuncBaseProps } from '../../glTasks/types';
 
+import { Canvas } from './styled';
+
 interface SceneProps {
   task: RootTasksType;
 }
@@ -15,8 +17,47 @@ const execTask = <K extends RootTasksType>(task: K, props: TaskFuncBaseProps) =>
   // Idk why func here may be undefined
   tasks[task]!(props);
 };
+
+const handleCanvasResize = (
+  gl: WebGLRenderingContext,
+  canvasRef: React.RefObject<HTMLCanvasElement>,
+) => {
+  let blocked = false;
+
+  const handleResize = () => {
+    if (blocked || canvasRef.current === null) {
+      return;
+    }
+
+    blocked = true;
+    setTimeout(() => {
+      blocked = false;
+    }, 2000);
+
+    const realToCSSPixels = window.devicePixelRatio;
+
+    const displayWidth = Math.floor(gl.canvas.clientWidth * realToCSSPixels);
+    const displayHeight = Math.floor(gl.canvas.clientHeight * realToCSSPixels);
+
+    canvasRef.current.width = displayWidth;
+    canvasRef.current.height = displayHeight;
+    console.log(displayWidth, displayHeight);
+
+    gl.viewport(0, 0, displayWidth, displayHeight);
+  };
+
+  window.addEventListener('resize', handleResize);
+  handleResize();
+
+  return () => {
+    window.removeEventListener('resize', handleResize);
+  };
+};
+
 export const Scene = (props: SceneProps) => {
   const canvasRef = React.useRef<HTMLCanvasElement>(null);
+
+  let gl: WebGLRenderingContext | null = null;
 
   React.useEffect(
     () => {
@@ -24,7 +65,7 @@ export const Scene = (props: SceneProps) => {
         return;
       }
 
-      const gl = canvasRef.current.getContext('webgl');
+      gl = canvasRef.current.getContext('webgl');
       if (gl === null) {
         throw Error('Failed to get canvas context');
       }
@@ -41,5 +82,12 @@ export const Scene = (props: SceneProps) => {
     [props.task],
   );
 
-  return <canvas ref={canvasRef}/>;
+  React.useEffect(() => gl !== null ? handleCanvasResize(gl, canvasRef) : undefined, [gl, canvasRef]);
+
+  return (
+    <Canvas
+      // @ts-ignore
+      ref={canvasRef}
+    />
+  );
 };
---
+++ src/components/Scene/styled.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Canvas = styled.canvas`
+  height: 100vh;
+  width: 100%;
+`;
